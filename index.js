3ec2106ade0a58c6675a7ab573a7e5be48bfd6df
tayga2410 <moldazhanov.eldar@gmail.com>
2024-10-05 11:32:56 +0500

    feat: adddedd update for pizzas, weapons count, addedd creation of a turtle

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 const sequelize = new Sequelize(
   config.database,
   config.username,
@@ -68,6 +70,64 @@ app.get("/favorite-pizzas", async (req, res) => {
   }
 });
 
+app.post('/add-turtle', async (req, res) => {
+  try {
+    const { name, color, weaponId, favoritePizzaId } = req.body;
+
+    const newTurtle = await db.turtles.create({
+      name,
+      color,
+      weaponId,
+      favoritePizzaId
+    });
+    
+    res.status(201).json(newTurtle);
+  } catch (error) {
+    console.error('Ошибка при создании черепашки:', error);
+    res.status(500).send('Ошибка при создании черепашки.');
+  }
+});
+
+app.put('/update-pizzas', async (req, res) => {
+  try {
+    const [updatedCount] = await db.pizzas.update(
+      { description: db.sequelize.fn('CONCAT', db.sequelize.col('description'), ' SUPER FAT!') }, 
+      {
+        where: {
+          calories: {
+            [db.Sequelize.Op.gt]: 3000, 
+          },
+        },
+      }
+    );
+
+    if (updatedCount > 0) {
+      res.status(200).json({ message: `${updatedCount} пицц(ы) обновлены.` });
+    } else {
+      res.status(404).json({ message: 'Пиццы с калориями больше 3000 не найдены.' });
+    }
+  } catch (error) {
+    console.error('Ошибка при обновлении пицц:', error);
+    res.status(500).json({ message: 'Ошибка при обновлении пицц.' });
+  }
+});
+
+app.get('/weapons-count', async (req, res) => {
+  try {
+    const count = await db.weapons.count({
+      where: {
+        dps: {
+          [db.Sequelize.Op.gte]: 100,
+        },
+      },
+    });
+
+    res.status(200).json({ count });
+  } catch (error) {
+    console.error('Ошибка при запросе количества оружий:', error);
+    res.status(500).json({ message: 'Ошибка при запросе количества оружий.' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
